0378f8571e53cd216c619c3c8e2a734ea54ed352
Alexandre <a.monnot13@gmail.com>
2017-09-18 16:08:48 +0200

    Hello world

---
+++ aMAZEd/aMAZEd/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aMAZEd
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("toto");
+        }
+    }
+}
