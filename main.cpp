67d1e4092bed38bdb6c8ff7a884cd39612d13e96
Alexandre <a.monnot13@gmail.com>
2017-09-20 17:54:45 +0200

    Quitte l'application lorsqu'on appuie sur Echap

--- main.cpp
+++ main.cpp
@@ -9,21 +9,21 @@ using namespace cv;
 int main(int argc, char** argv)
  {
 
-     VideoCapture cap(0); // open the default camera
-     if(!cap.isOpened())  // check if we succeeded
+     VideoCapture capture(0); // open the default camera
+     if(!capture.isOpened())  // check if we succeeded
          return -1;
 
      Mat edges;
-     namedWindow("edges",1);
+     namedWindow("Capture",1);
      while(true)
      {
          Mat frame;
-         cap >> frame; // get a new frame from camera
-         //cvtColor(frame, edges, CV_BGR2GRAY);
-         //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-         //Canny(edges, edges, 0, 30, 3);
-         imshow("edges", frame);
-         waitKey(30);
+         capture >> frame; // get a new frame from camera
+         
+         imshow("Capture", frame);
+
+         // Quitte l'application lorsqu'on appuie sur Echap
+         if(waitKey(30) == 27) break;
      }
 
      // the camera will be deinitialized automatically in VideoCapture destructor
