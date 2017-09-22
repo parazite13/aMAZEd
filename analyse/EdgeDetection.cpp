c33f7589416bb435f9966dfea49c2a742a8f3376
THIBAUD-PC\Thomas <tgrisel@outlook.com>
2017-09-22 10:05:18 +0200

    Détection de coins

---
+++ analyse/EdgeDetection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Thomas on 22/09/2017.
+//
+
+#include "edgeDetection.h"
---
+++ analyse/EdgeDetection.h
@@ -0,0 +1,14 @@
+//
+// Created by Thomas on 22/09/2017.
+//
+
+#ifndef PROJET_AMAZED_EDGEDETECTION_H
+#define PROJET_AMAZED_EDGEDETECTION_H
+
+
+class edgeDetection {
+
+};
+
+
+#endif //PROJET_AMAZED_EDGEDETECTION_H
