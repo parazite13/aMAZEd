b0d4478f8cf327c249800035f18b8cb66fe99a63
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-09-22 10:41:12 +0200

    Ajout classe CameraStream + image inversée

--- main.cpp
+++ main.cpp
@@ -2,31 +2,28 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include "stream/CameraStream.h"
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv)
- {
+int main(int argc, char** argv){
 
-     VideoCapture capture(0); // open the default camera
-     if(!capture.isOpened())  // check if we succeeded
-         return -1;
+    CameraStream fluxCamera = CameraStream();
+    if(!fluxCamera.isOpen())
+        return -1;
 
-     Mat edges;
-     namedWindow("Capture",1);
-     while(true)
-     {
-         Mat frame;
-         capture >> frame; // get a new frame from camera
-         
-         imshow("Capture", frame);
+   // fluxCamera.showCamera();
+    Mat frame;
+    while (true){
+        frame = fluxCamera.getCurrentFrame();
 
-         // Quitte l'application lorsqu'on appuie sur Echap
-         if(waitKey(30) == 27) break;
-     }
+        namedWindow("Frame",WINDOW_AUTOSIZE);
+        imshow("Frame", frame);
+        if(waitKey(30) == 27) break;
+    }
 
-     // the camera will be deinitialized automatically in VideoCapture destructor
-     return 0;
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 
 }
