4eeb65fe2f608d96d68c5508b51ed0a552ce36c8
THIBAUD-PC\Thomas <tgrisel@outlook.com>
2017-09-22 15:57:06 +0200

    detection lignes

--- analyse/EdgeDetection.cpp
+++ analyse/EdgeDetection.cpp
@@ -35,3 +35,41 @@ void EdgeDetection::cornersDetection(cv::Mat img, int thresh/*, cv::Point coord[
         }
     }
 }
+
+
+void EdgeDetection::linesDetection(cv::Mat img, int thresh){
+    /// détection des contours avec canny
+    cv::Mat img2;
+    cv::Canny(img, img2, 50, thresh, 3);
+
+    /// detection des lignes dans le vect lines
+  /* std::vector<cv::Vec2f> lines;
+    HoughLines( img2, lines, 1, CV_PI/180, 100 );
+
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0];
+        float theta = lines[i][1];
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        cv::Point pt1(cvRound(x0 + 1000*(-b)),
+                  cvRound(y0 + 1000*(a)));
+        cv::Point pt2(cvRound(x0 - 1000*(-b)),
+                  cvRound(y0 - 1000*(a)));
+        line( img, pt1, pt2, cv::Scalar(0,0,255), 3, 8 );
+    }*/
+    std::vector<cv::Vec4i> lines;
+    /// houghLinesP(imgsource,
+    /// vectdest,
+    /// distance resolution en pixels
+    /// angle resolution en rad
+    /// seuil
+    /// min longeur
+    /// max ecart entre pixels de la ligne)
+    HoughLinesP(img2, lines, 1, CV_PI/180, 100, 50, 25 );
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        cv::Vec4i l = lines[i];
+        cv::line( img, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 1, CV_AA);
+    }
+}
--- analyse/EdgeDetection.h
+++ analyse/EdgeDetection.h
@@ -9,12 +9,19 @@
 class EdgeDetection {
 
 public:
-    /* détecte les angles dans une image
+    /* détecte les angles dans une image et les affiche
      * @params
      *      img : l'image où trouver les angles
      *      thresh : seuil de détection
      * */
     static void cornersDetection(cv::Mat img, int thresh/*, cv::Point coord[]*/);
+
+    /* détecte les lignes dans une image et les affiche
+     * @params
+     *      img : l'image où trouver les lignes
+     *      thresh : seuil de détection
+     * */
+    static void linesDetection(cv::Mat img, int thresh);
 
 };
 
