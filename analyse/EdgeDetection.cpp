2f518acfdcdf7fd6dec7e4fd22af4e63bf39a961
THIBAUD-PC\Thomas <tgrisel@outlook.com>
2017-09-25 11:34:53 +0200

    commentaires / biblios

--- analyse/EdgeDetection.cpp
+++ analyse/EdgeDetection.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include "EdgeDetection.h"
 
+using namespace std;
+using namespace cv;
 /*
-void EdgeDetection::cornersDetection(cv::Mat img, int thresh){
-    cv::Mat imgGrey;
-    cv::Mat imgHLS;
+void EdgeDetection::cornersDetection(Mat img, int thresh){
+    Mat imgGrey;
+    Mat imgHLS;
     int nbCorner = 0;
     ///conversion de l'image en niveaux de gris
-    cv::cvtColor( img, imgGrey, CV_BGR2GRAY );
+    cvtColor( img, imgGrey, CV_BGR2GRAY );
     ///conversion de RGB à HLS
-    cv::cvtColor( img, imgHLS, CV_BGR2HLS );
+    cvtColor( img, imgHLS, CV_BGR2HLS );
 
     ///On split les canaux
-    std::vector<cv::Mat> canaux;
-    cv::split(imgHLS,canaux);
+    vector<Mat> canaux;
+    split(imgHLS,canaux);
 
 
-    cv::Mat dst, dst_norm, dst_norm_scaled;
-    dst = cv::Mat::zeros( img.size(), CV_32FC1 );  //init
+    Mat dst, dst_norm, dst_norm_scaled;
+    dst = Mat::zeros( img.size(), CV_32FC1 );  //init
 
     /// Detector parameters
     int blockSize = 2;
@@ -25,22 +27,22 @@ void EdgeDetection::cornersDetection(cv::Mat img, int thresh){
     double k = 0.04;
 
     /// Detecting corners
-    cornerHarris( imgGrey, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT );
+    cornerHarris( imgGrey, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
 
     /// Normalizing
-    normalize( dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat() );
+    normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
     convertScaleAbs( dst_norm, dst_norm_scaled );
 
     for( int j = 0; j < dst_norm.rows ; j++ ){
         for( int i = 0; i < dst_norm.cols; i++ ){
             if( (int) dst_norm.at<float>(j,i) > thresh ){
 
-                coord[nbCorner] = cv::Point(i, j);
+                coord[nbCorner] = Point(i, j);
                 nbCorner++;
 
                 if((int)canaux[1].at<uchar>(j,i) <= 185 && (int)canaux[1].at<uchar>(j,i) >= 165) {
-                    circle(img, cv::Point(i, j), 5, cv::Scalar(0), 2, 8, 0);
-                    //std::cout << (int)canaux[0].at<uchar>(j,i) << std::endl;
+                    circle(img, Point(i, j), 5, Scalar(0), 2, 8, 0);
+                    //cout << (int)canaux[0].at<uchar>(j,i) << endl;
                 }
 
             }
@@ -49,19 +51,19 @@ void EdgeDetection::cornersDetection(cv::Mat img, int thresh){
 }
 
 */
-std::vector<std::vector<cv::Point2f>> EdgeDetection::linesDetection(cv::Mat img, int thresh){
+vector<vector<Point2f>> EdgeDetection::linesDetection(Mat img, int thresh){
     /// détection des contours avec Canny
-    cv::Mat imgCanny;
-    cv::Canny(img, imgCanny, 100, 200, 3);
+    Mat imgCanny;
+    Canny(img, imgCanny, 100, 200, 3);
 
-//    cv::namedWindow("Canny",cv::WINDOW_AUTOSIZE);
-//    cv::imshow("Canny", imgCanny);
+    namedWindow("Canny",WINDOW_AUTOSIZE);
+    imshow("Canny", imgCanny);
 
     /// detection des lignes dans le vect lines
 
     /// vecteur dans lequel sont stockées les lignes
     ///     lignes stockées sous la forme (x1,y1,x2,y2)
-    std::vector<cv::Vec4i> lines;
+    vector<Vec4i> lines;
 
     /// houghLinesP(imgsource,
     /// vectdest,
@@ -73,22 +75,22 @@ std::vector<std::vector<cv::Point2f>> EdgeDetection::linesDetection(cv::Mat img,
     HoughLinesP(imgCanny, lines, 1, CV_PI/180, 100, 20, 25);
 
     /// tableau de couples de points
-    std::vector<std::vector<cv::Point2f>> vectLines;
+    vector<vector<Point2f>> vectLines;
 
     for( size_t i = 0; i < lines.size(); i++ ){
         /// récupération d'une ligne
-        cv::Vec4i l = lines[i];
+        Vec4i l = lines[i];
 
         /// couple de points
-        std::vector<cv::Point2f> vectPoints ;
-        vectPoints.push_back(cv::Point2f(l[0], l[1]));
-        vectPoints.push_back(cv::Point2f(l[2], l[3]));
+        vector<Point2f> vectPoints ;
+        vectPoints.push_back(Point2f(l[0], l[1]));
+        vectPoints.push_back(Point2f(l[2], l[3]));
 
         /// ajout du couple au tableau
         vectLines.push_back(vectPoints) ;
 
         ///tracé de la ligne
-        cv::line( img, vectPoints[0], vectPoints[1], cv::Scalar(0,0,255), 1, CV_AA);
+        line( img, vectPoints[0], vectPoints[1], Scalar(0,0,255), 1, CV_AA);
     }
 
     return(vectLines);
@@ -96,23 +98,23 @@ std::vector<std::vector<cv::Point2f>> EdgeDetection::linesDetection(cv::Mat img,
 
 
 
-void EdgeDetection::colorDetection(cv::Mat img) {
+void EdgeDetection::colorDetection(Mat img) {
     ///Conversion en hsv
-    cv::Mat hsv;
-    cv::cvtColor(img, hsv, CV_BGR2HSV);
+    Mat hsv;
+    cvtColor(img, hsv, CV_BGR2HSV);
 
     ///Création d'un masque
-    cv::Mat mask;
+    Mat mask;
 
     ///Si le pixel de l'image original à un h et un s proche de celui recharché on colorie l'image "mask" en blanc, autrement en noir
     int h = 346;
     int s = 83;
     int toleranceh = 4;
     int tolerances = 17;
-    cv::inRange(hsv, cv::Scalar(h-toleranceh-1, s-tolerances, 0), cv::Scalar(h+toleranceh -1, s+tolerances, 255), mask);
+    inRange(hsv, Scalar(h-toleranceh-1, s-tolerances, 0), Scalar(h+toleranceh -1, s+tolerances, 255), mask);
 
 
-    cv::namedWindow("1",cv::WINDOW_AUTOSIZE);
-    cv::imshow("1", mask);
+    namedWindow("1",WINDOW_AUTOSIZE);
+    imshow("1", mask);
 
 }
