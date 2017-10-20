f808ccb9afbf329ff5d5119227669b005d3e6aeb
Alexandre <a.monnot13@gmail.com>
2017-10-20 16:06:27 +0200

    Renmage variable FPS

--- modelisation/OpenGL.cpp
+++ modelisation/OpenGL.cpp
@@ -78,7 +78,7 @@ void OpenGL::CallBackDisplayFunc(){
 
     glutSwapBuffers();
 
-    glutTimerFunc((unsigned int)1000 / FPS, loop, 0);
+    glutTimerFunc((unsigned int)1000 / MAX_FPS, loop, 0);
 }
 
 void OpenGL::CallBackReshapeFunc(int w, int h){
