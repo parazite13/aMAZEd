86befd9fab971d0a50374bd3c7462c32fb2f46b8
Fabien <alt.f4413@gmail.com>
2017-10-23 08:38:32 +0200

    Le push précédent n'a pas tout ajouté...

--- physics/Vector2d.h
+++ physics/Vector2d.h
@@ -1,13 +1,60 @@
-//
-// Created by Fabien on 2017/10/22.
-//
 
 #ifndef AMAZED_VECTOR2D_H
 #define AMAZED_VECTOR2D_H
 
 
 class Vector2d {
+    private:
+        double _x1, _y1, _x2, _y2;
+        double _X, _Y;        // pour les calculs
 
+    public:
+        /// constructeur
+        Vector2d(double x1, double y1, double x2, double y2);
+        Vector2d(double X, double Y);
+
+        /// Projette le vecteur sur v2
+        double projectionOn(Vector2d v2);
+
+        ///Calcule la norme d'un vecteur
+        double getMagnitude();
+
+        /**
+        * Method to obtain vector unit of current vector
+        * @return A copy of normalised vector
+        */
+        Vector2d normalize();
+
+        /**
+         *  Les vecteurs doivent etre de la meme taille
+         * @param vect
+         * @return Le produit vectoriel des 2 vecteurs
+         */
+        double dotProduct(Vector2d vect);
+
+        double get_x1() const;
+
+        void set_x1(double _x1);
+
+        double get_y1() const;
+
+        void set_y1(double _y1);
+
+        double get_x2() const;
+
+        void set_x2(double _x2);
+
+        double get_y2() const;
+
+        void set_y2(double _y2);
+
+        double get_X() const;
+
+        void set_X(double _X);
+
+        double get_Y() const;
+
+        void set_Y(double _Y);
 };
 
 
